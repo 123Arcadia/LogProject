1d1064f722c15b5821203240961062b1788f3ebc
123Arcadia <1119045415@qq.com>
2023-12-02 00:33:14 +0800

    eclipse导出jar几个log4j2.xml不起作用：必须log4j2等xml文件和jar保持同目录下

--- README.md
+++ README.md
@@ -26,4 +26,17 @@ Async-Log 使用slf4j+log4j2实现异步log打印
           <artifactId>disruptor</artifactId>
           <version>3.3.7</version>
       </dependency>
+
+      <!--注意:要排除slg4j-log4j12这个依赖-->
 ```
+### 踩坑记录:
+
+1. 为什么使用eclipse导出的jar运行没有采用resources下的`log4j2.xml`配置输出?
+
+**解决**: eclipse似乎对jar运行后resources下文件不起作用，把`log4j2.xml`和`log4j2.component.properties`必须和jar保持同目录下
+    在运行命令:
+
+```shell
+> java -jar -Xms1024m -Xmx1536m -XX:PermSize=128MXX:MaxPermSize=256M ***.jar
+```
+
--- src/main/java/org/cyan/App.java
+++ src/main/java/org/cyan/App.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.core.async.AsyncLoggerContextSelector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 /**
  * Hello world!
  * 从低到高依次是 trace < debug < info < warn < error
@@ -16,7 +17,7 @@ public class App {
         long start = System.currentTimeMillis();
         for (int i = 0; i < 1_000_000/2; i++) {
             logger.error("[error]Hello World!"+i);
-            logger.warn("[warn] Hello zcw!");
+            logger.info("[info] Hello zcw!");
             System.out.println("Hello World! " + i);
         }
         System.out.println("耗时: " + (System.currentTimeMillis() - start) + " ms");
@@ -26,5 +27,8 @@ public class App {
         // 可以查看是否为异步日志
         logger.info("是否为异步日志：{}", AsyncLoggerContextSelector.isSelected());
         // 是否为异步日志：true
+        System.out.println("log Async:"+AsyncLoggerContextSelector.isSelected());
+        // log Async:true
     }
+
 }
